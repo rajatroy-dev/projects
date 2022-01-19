ee7a0ac2bf8f63409140f491eda4490c99ba5f70
Rajat Roy <rajatroy.dev@gmail.com>
2022-01-19 23:00:32 +0530

    ProjectsL websites: me-clone - Flicker live classes | announcements

--- me-clone/tailwind.config.js
+++ me-clone/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flicker: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
