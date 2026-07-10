7314eb7107b53bf491827667b349eb148a49f96a
Rajat Roy <rajatroy.dev@gmail.com>
2026-07-10 23:10:47 +0530

    WIP Planning working with credit cards

---
+++ cc_payment_reminder/card_manager.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+CLI to manage credit card bill reminders.
+
+Usage:
+    python card_manager.py add
+    python card_manager.py list
+    python card_manager.py edit <id>
+    python card_manager.py delete <id>
+    python card_manager.py paid <id>
+"""
